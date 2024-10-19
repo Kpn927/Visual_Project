f86f6b4f206c37ebf0423bc0f108f52629785ade
Sebangel <angeljavicueche@gmail.com>
2024-10-18 21:46:26 -0400

    feat: componente de conexión a DB

--- threads/src/threads/Clase1.java
+++ threads/src/threads/Clase1.java
@@ -2,50 +2,27 @@ package threads;
 
 // Imports necesarios para la ejeccución con la base de datos //
 
-import java.sql.*;
 
 public class Clase1 {
 	
 static class Hilo extends Thread {
-	
-		// VARIABLES A CAMBIAR DEPENDIENDO DE LA BASE DE DATOS. //
-		private String URL = "jdbc:postgresql://localhost:5432/sakila";
-		private String USER = "postgres";
-		private String CLAVE = "admin";
-		private Connection conn;
+		
 		private static int hilos_fallados = 0, hilos_funcionando = 0;
         
         // se utiliza @OVERRIDE RUN Para sobre escribir la función que está en THREAD ya definida.
         @Override
         public void run() {
             try {
-            	conn = DriverManager.getConnection(URL, USER, CLAVE);
-                if (conn != null) {
-                	
-                    Statement st = conn.createStatement();
-                    st.setFetchSize(0);
-                    
-                    ResultSet rs = st.executeQuery("SELECT * FROM actor LIMIT 1;");
-                    while(rs.next()) {
-                    	
-                    	System.out.print(rs.getString(1) + "  ");
-                    	System.out.println(rs.getString(2));
-                    
-                    }
+            	DBConnection conn = new DBConnection();
+            	getProperties config = new getProperties("DB.properties");
+        		String query = config.obtenerPropiedad("query");
+            	conn.exeQuery(query);
                     
                    synchronized(Hilo.class) {
                 	    // SE USA SYNCHRONIZED PARA EVITAR CASOS DE MULTIPLES HILOS INTENTANDO ACCEDER AL INCREMENTO DE ESTE.
                     	hilos_funcionando++;
                     }
-                    
-                    st.close();
-                    rs.close();
-                    conn.close();
-                    
-                } else {
-                    System.out.println("ERROR: La conexión ha fallado.");
-                }	
-            } catch (SQLException e) {
+            } catch (Error e) {
             	// SE USA SYNCHRONIZED PARA EVITAR CASOS DE MULTIPLES HILOS INTENTANDO ACCEDER AL INCREMENTO DE ESTE.
             	e.printStackTrace();
             	synchronized(Hilo.class) {
@@ -69,19 +46,11 @@ static class Hilo extends Thread {
     }
 	
 	public static void main(String[] args) {
-		getProperties config = new getProperties("DB.properties");
-		String valor = config.obtenerPropiedad("user");
-		
-		System.out.println("El valor de miPropiedad es: " + valor);
 				
 			int[] cantidad_funcionando = new int[4];
 			int[] cantidad_fallados = new int[4];
 			long[] times = new long[5];
 			
-			
-			/*for (int i = 0; i < 5; i++) {
-			    times[i] -= System.currentTimeMillis();
-			}*/
 			
 			String numero_hilos = "1";
 			System.out.println("prueba 1");
@@ -110,76 +79,21 @@ static class Hilo extends Thread {
 		        	}
 				}
 				
+				
                 cantidad_funcionando[i] = Hilo.getHilosCompletados();
                 cantidad_fallados[i] = Hilo.getHilosFallados();
                 times[i] += System.currentTimeMillis();
 				
 			}
 			
-			/*try {
-        		Thread.sleep(10000);
-        	}catch(InterruptedException e) {
-        		e.printStackTrace();
-        	}*/
-			
 			System.out.println("prueba 3");
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
-        	/*for (int i = 1; i <= 1000; i++){
-            	Hilo hilo = new Hilo();
-            	hilo.start();
-            	
-            	switch (i) {
-                case 10:
-                	try {
-						hilo.join();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-                    cantidad_funcionando[0] = Hilo.getHilosCompletados();
-                    cantidad_fallados[0] = Hilo.getHilosFallados();
-                    times[0] += System.currentTimeMillis();
-                    break;
-                case 100:
-					try {
-						hilo.join();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-                    cantidad_funcionando[1] = Hilo.getHilosCompletados();
-                    cantidad_fallados[1] = Hilo.getHilosFallados();
-                    times[1] += System.currentTimeMillis();
-                    break;
-                case 1000:
-					try {
-						hilo.join();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-                    cantidad_funcionando[2] = Hilo.getHilosCompletados();
-                    cantidad_fallados[2] = Hilo.getHilosFallados();
-                    times[2] += System.currentTimeMillis();
-                    break;
-                case 10000:
-					try {
-						hilo.join();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-                    cantidad_funcionando[3] = Hilo.getHilosCompletados();
-                    cantidad_fallados[3] = Hilo.getHilosFallados();
-                    times[3] += System.currentTimeMillis();
-                    break;
-                case 100000:
-                    times[4] += System.currentTimeMillis();
-                    break;
-            	}
-            }
-        	
-        	try {
-        		Thread.sleep(5000);
-        	}catch(InterruptedException e) {
-        		e.printStackTrace();
-        	}*/
 
         	System.out.println("FUNCIONAMIENTOl " + "Completados " + " l " + "Fallados" + " l " + "Tiempo " + "(ms)");
         	System.out.println("10 HILOS      l " + cantidad_funcionando[0] + " l " + cantidad_fallados[0] + " l " + times[0] + "ms");
--- threads/src/threads/DBConnection.java
+++ threads/src/threads/DBConnection.java
@@ -3,5 +3,44 @@ package threads;
 import java.sql.*;
 
 public class DBConnection {
+	private Connection conn;
+	String user;
+	String url;
+	String password;
+	
+	public void exeQuery(String query) {
+		try {
+        	conn = DriverManager.getConnection(url, user, password);
+            if (conn != null) {
+            	
+                Statement st = conn.createStatement();
+                st.setFetchSize(0);
+                
+                ResultSet rs = st.executeQuery(query);
+                while(rs.next()) {
+                	
+                	System.out.print(rs.getString(1) + "  ");
+                	System.out.println(rs.getString(2));
+                
+                }
+                st.close();
+                rs.close();
+                conn.close();
+                
+            } else {
+                System.out.println("ERROR: La conexión ha fallado.");
+            }	
+        } catch (SQLException e) {
+        	throw new Error(e.getMessage());
+        }	
+	}
+	
+	public DBConnection(){
+		getProperties config = new getProperties("DB.properties");
+		user = config.obtenerPropiedad("user");
+		url = config.obtenerPropiedad("DBurl");
+		password = config.obtenerPropiedad("password");
+		
+	}
 	
 }
